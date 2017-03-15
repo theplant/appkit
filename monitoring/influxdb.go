5d0c8e1507cc7dac093c15e0f51c9245f8e19ca9
bodhi <bodhi@theplant.jp>
2017-03-15 15:08:05 +1100

    Introduce monitoring package
    Simple monitoring/instrumentation extracted from client
    project. Provides middleware to track requests, and installs a monitor
    in the request context to expose the monitor for use in later
    http.Handlers for arbitrary metrics.
    
    Part of #4.

---
+++ monitoring/influxdb.go
@@ -0,0 +1,131 @@
+package monitoring
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	influxdb "github.com/influxdata/influxdb/client"
+	"github.com/pkg/errors"
+	"github.com/theplant/appkit/log"
+)
+
+// InfluxMonitorConfig type for configuration of Monitor that sinks to
+// InfluxDB
+type InfluxMonitorConfig string
+
+// NewInfluxdbMonitor creates new monitoring influxdb
+// client. config URL syntax is `https://<username>:<password>@<influxDB host>/<database>`
+//
+// Will returns a error if monitorURL is invalid or not absolute.
+//
+// Will not return error if InfluxDB is unavailable, but the returned
+// Monitor will log errors if it cannot push metrics into InfluxDB
+func NewInfluxdbMonitor(config InfluxMonitorConfig, logger log.Logger) (Monitor, error) {
+	monitorURL := string(config)
+
+	u, err := url.Parse(monitorURL)
+	if err != nil {
+		return nil, errors.Wrapf(err, "couldn't parse influxdb url %v", monitorURL)
+	} else if !u.IsAbs() {
+		return nil, errors.Errorf("influxdb monitoring url %v not absolute url", monitorURL)
+	}
+
+	// NewClient always returns a nil error
+	client, _ := influxdb.NewClient(influxdb.Config{
+		URL: *u,
+	})
+
+	monitor := influxdbMonitor{
+		database: strings.TrimLeft(u.Path, "/"),
+		client:   client,
+		logger:   logger,
+	}
+
+	logger = logger.With(
+		"scheme", u.Scheme,
+		"username", u.User.Username(),
+		"database", monitor.database,
+		"host", u.Host,
+	)
+
+	// check connectivity to InfluxDB every 5 minutes
+	go func() {
+		t := time.NewTimer(5 * time.Minute)
+
+		for {
+			// Ignore duration, version
+			_, _, err = client.Ping()
+			if err != nil {
+				logger.Warn().Log(
+					"err", err,
+					"during", "influxdb.Client.Ping",
+					"msg", fmt.Sprintf("couldn't ping influxdb: %v", err),
+				)
+			}
+
+			<-t.C
+		}
+	}()
+
+	logger.Info().Log(
+		"msg", fmt.Sprintf("influxdb instrumentation writing to %s://%s@%s/%s", u.Scheme, u.User.Username(), u.Host, monitor.database),
+	)
+
+	return &monitor, nil
+}
+
+// InfluxdbMonitor implements monitor.Monitor interface, it wraps
+// the influxdb client configuration.
+type influxdbMonitor struct {
+	client   *influxdb.Client
+	database string
+	logger   log.Logger
+}
+
+// InsertRecord part of monitor.Monitor.
+func (im influxdbMonitor) InsertRecord(measurement string, value interface{}, tags map[string]string, at time.Time) {
+	// Ignore response, we only care about write errors
+	_, err := im.client.Write(influxdb.BatchPoints{
+		Database: im.database,
+		Points: []influxdb.Point{
+			{
+				Measurement: measurement,
+				Fields: map[string]interface{}{
+					"value": value,
+				},
+				Tags: tags,
+				Time: at,
+			},
+		},
+	})
+
+	if err != nil {
+		im.logger.Error().Log(
+			"err", err,
+			"database", im.database,
+			"measurement", measurement,
+			"value", value,
+			"tags", tags,
+			"during", "influxdb.Client.Write",
+			"msg", fmt.Sprintf("Error inserting record into %s: %v", measurement, err),
+		)
+	}
+}
+
+func (im influxdbMonitor) Count(measurement string, value float64, tags map[string]string) {
+	im.InsertRecord(measurement, value, tags, time.Now())
+}
+
+// CountError logs a value in measurement, with the given error's
+// message stored in an `error` tag.
+func (im influxdbMonitor) CountError(measurement string, value float64, err error) {
+	data := map[string]string{"error": err.Error()}
+	im.Count(measurement, value, data)
+}
+
+// CountSimple logs a value in measurement (with no tags).
+func (im influxdbMonitor) CountSimple(measurement string, value float64) {
+	im.Count(measurement, value, nil)
+}
---
+++ monitoring/middleware.go
@@ -0,0 +1,95 @@
+package monitoring
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/theplant/appkit/contexts"
+	"github.com/theplant/appkit/contexts/trace"
+	"github.com/theplant/appkit/log"
+)
+
+type key int
+
+const monitorKey key = iota
+
+// WithMonitor wraps the given http.Handler to:
+// 1. instrument requests via a Monitor
+// 2. install monitor in request context for use by later handlers
+func WithMonitor(m Monitor) func(h http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			defer func() {
+				interval := time.Now().Sub(start)
+				go func() {
+					tags := tagsForContext(r)
+					m.InsertRecord("request", float64(interval/time.Millisecond), tags, start)
+
+				}()
+			}()
+
+			h.ServeHTTP(w, r.WithContext(Context(r.Context(), m)))
+		})
+	}
+}
+
+// MonitorContext installs a given Monitor in the returned context
+func Context(c context.Context, m Monitor) context.Context {
+	return context.WithValue(c, monitorKey, m)
+}
+
+// ForceContext extracts a Monitor from a (possibly nil) context, or
+// returns a NewLogMonitor using a log from the context or
+// log.Default()
+func ForceContext(ctx context.Context) Monitor {
+	var logger log.Logger
+	if ctx != nil {
+		val := ctx.Value(monitorKey)
+		if monitor, ok := val.(Monitor); ok {
+			return monitor
+		}
+
+		logger = log.ForceContext(ctx)
+	} else {
+		logger = log.Default()
+	}
+	return NewLogMonitor(logger)
+}
+
+func tagsForContext(r *http.Request) map[string]string {
+	path := scrubPath(r.URL.Path)
+	tags := map[string]string{
+		"path":           path,
+		"request_method": r.Method,
+	}
+
+	ctx := r.Context()
+
+	if reqID, ok := trace.RequestTrace(ctx); ok {
+		tags["req_id"] = fmt.Sprintf("%v", reqID)
+	}
+
+	if status, ok := contexts.HTTPStatus(ctx); ok {
+		tags["response_code"] = strconv.Itoa(status)
+	} else {
+		log.ForceContext(ctx).Warn().Log(
+			"msg", fmt.Sprintf("cannot determine response code for %s %s (perhaps no WithHTTPStatus in context?)", r.Method, path),
+			"path", path,
+			"method", r.Method,
+		)
+	}
+
+	return tags
+}
+
+var idScrubber = regexp.MustCompile("[0-9]+")
+
+func scrubPath(path string) string {
+	return idScrubber.ReplaceAllString(path, ":id")
+}
