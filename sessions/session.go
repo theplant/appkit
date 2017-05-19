85af77d3a6fa1dd5186d93b14f08e1277fadc455
Raven <enn.raven@gmail.com>
2017-05-19 11:18:04 +0800

    Eliminate global variables.

--- sessions/session.go
+++ sessions/session.go
@@ -9,12 +9,11 @@ import (
 	"github.com/gorilla/sessions"
 )
 
-var sessionStore *sessions.CookieStore
-
 type session struct {
 	w      http.ResponseWriter
 	r      *http.Request
 	config *Config
+	store  *sessions.CookieStore
 }
 
 // Config the necessary configs for session
@@ -23,25 +22,6 @@ type Config struct {
 	Key    string
 	MaxAge int
 	Secure bool
-}
-
-func (s session) setupSession() {
-	if s.config.Name == "" {
-		panic("session name must be present")
-	}
-
-	sessionStoreKey, err := base64.StdEncoding.DecodeString(s.config.Key)
-	if err != nil {
-		panic(err)
-	}
-
-	sessionStore = sessions.NewCookieStore(sessionStoreKey)
-
-	sessionStore.Options.HttpOnly = true
-	sessionStore.Options.Secure = s.config.Secure
-	if s.config.MaxAge != 0 {
-		sessionStore.MaxAge(s.config.MaxAge)
-	}
 }
 
 type sessionContextKey int
@@ -53,8 +33,9 @@ const sessionCtxKey sessionContextKey = iota
 func WithSession(conf *Config) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			se := newSession(w, r, conf)
-			se.setupSession()
+			store := setupSessionStore(conf)
+			se := newSession(w, r, conf, store)
+
 			// Record session storer in context
 			ctx := context.WithValue(r.Context(), sessionCtxKey, se)
 
@@ -64,22 +45,11 @@ func WithSession(conf *Config) func(http.Handler) http.Handler {
 		})
 	}
 }
-func newSession(w http.ResponseWriter, r *http.Request, config *Config) *session {
-	return &session{w, r, config}
-}
-
-func getSession(ctx context.Context) *session {
-	if se, ok := ctx.Value(sessionCtxKey).(*session); ok {
-		return se
-	}
-
-	panic("Cannot get session in the request, please make sure the WithSession middleware has been executed before calling this function.")
-}
 
 // Get Get value of the given key in the session.
 func Get(ctx context.Context, key string) (string, error) {
 	s := getSession(ctx)
-	session, err := sessionStore.Get(s.r, s.config.Name)
+	session, err := s.store.Get(s.r, s.config.Name)
 	if err != nil {
 		return "", err
 	}
@@ -100,7 +70,7 @@ func Get(ctx context.Context, key string) (string, error) {
 // Put put key-value map into the session
 func Put(ctx context.Context, key, value string) error {
 	s := getSession(ctx)
-	session, err := sessionStore.Get(s.r, s.config.Name)
+	session, err := s.store.Get(s.r, s.config.Name)
 	if err != nil {
 		return err
 	}
@@ -113,7 +83,7 @@ func Put(ctx context.Context, key, value string) error {
 // Del delete value from the session by given key
 func Del(ctx context.Context, key string) error {
 	s := getSession(ctx)
-	session, err := sessionStore.Get(s.r, s.config.Name)
+	session, err := s.store.Get(s.r, s.config.Name)
 	if err != nil {
 		return err
 	}
@@ -122,4 +92,37 @@ func Del(ctx context.Context, key string) error {
 	session.Save(s.r, s.w)
 
 	return nil
+}
+
+func setupSessionStore(config *Config) *sessions.CookieStore {
+	if config.Name == "" {
+		panic("session name must be present")
+	}
+
+	sessionStoreKey, err := base64.StdEncoding.DecodeString(config.Key)
+	if err != nil {
+		panic(err)
+	}
+
+	sessionStore := sessions.NewCookieStore(sessionStoreKey)
+
+	sessionStore.Options.HttpOnly = true
+	sessionStore.Options.Secure = config.Secure
+	if config.MaxAge != 0 {
+		sessionStore.MaxAge(config.MaxAge)
+	}
+
+	return sessionStore
+}
+
+func newSession(w http.ResponseWriter, r *http.Request, config *Config, sessionStore *sessions.CookieStore) *session {
+	return &session{w, r, config, sessionStore}
+}
+
+func getSession(ctx context.Context) *session {
+	if se, ok := ctx.Value(sessionCtxKey).(*session); ok {
+		return se
+	}
+
+	panic("Cannot get session in the request, please make sure the WithSession middleware has been executed before calling this function.")
 }
