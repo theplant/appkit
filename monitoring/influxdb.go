7ffce46f21d0f50c3285d83af4804e55fc698f75
bodhi <bodhi@theplant.jp>
2019-01-31 14:29:37 +1100

    Allow controlled shutdown of InfluxDB monitoring buffer goroutine
    `NewInfluxdbMonitor` now returns a "close" `func()` that will gracefully
    terminate the buffer goroutine when called.
    
    There is still a window where data can be lost, if clients continue
    writing points *after* the close function is called. An example of
    this is in the monitoring middleware:
    
    0. Monitor is running on goroutine J
    1. HTTP server is shut down, goroutine G that initiated shutdown is
       blocked on a request still in progress on goroutine H.
    2. Request on H completes
    2.1 H spawns goroutine I to push point onto buffer
    2.2 H exits
    3. G becomes unblocked (as H exited), closes monitor
    4. J notices monitor closure, flushes buffer and exits
    5. I pushes point onto buffer (but buffer was closed in step 4).

--- monitoring/influxdb.go
+++ monitoring/influxdb.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	influxdb "github.com/influxdata/influxdb1-client/v2"
@@ -137,14 +138,21 @@ func parseInfluxMonitorConfig(config InfluxMonitorConfig) (*influxMonitorCfg, er
 // max-buffer-size is optional, default is 10000, it must > buffer-size,
 //   if the batch write fails and buffered size reach max-buffer-size then clean up the buffer (mean the data is lost).
 //
-// Will returns a error if monitorURL is invalid or not absolute.
+// The second return value is a function that will cause the batching
+// goroutine to write buffered points, then terminate. This function
+// will block until one attempt to flush the buffer completes (either
+// success or failure).
 //
-// Will not return error if InfluxDB is unavailable, but the returned
-// Monitor will log errors if it cannot push metrics into InfluxDB
-func NewInfluxdbMonitor(config InfluxMonitorConfig, logger log.Logger) (Monitor, error) {
+// The third return value will be non-nil if monitorURL is invalid or
+// not absolute.
+//
+// This function will not return error if InfluxDB is unavailable, but
+// the returned Monitor will log errors if it cannot push metrics into
+// InfluxDB.
+func NewInfluxdbMonitor(config InfluxMonitorConfig, logger log.Logger) (Monitor, func(), error) {
 	cfg, err := parseInfluxMonitorConfig(config)
 	if err != nil {
-		return nil, err
+		return nil, func() {}, err
 	}
 
 	httpConfig := influxdb.HTTPConfig{
@@ -156,8 +164,10 @@ func NewInfluxdbMonitor(config InfluxMonitorConfig, logger log.Logger) (Monitor,
 	client, err := influxdb.NewHTTPClient(httpConfig)
 
 	if err != nil {
-		return nil, errors.Wrapf(err, "couldn't initialize influxdb http client with http config %+v", httpConfig)
+		return nil, func() {}, errors.Wrapf(err, "couldn't initialize influxdb http client with http config %+v", httpConfig)
 	}
+
+	logger = logger.With("context", "appkit/monitoring.influxdb")
 
 	monitor := &influxdbMonitor{
 		database: cfg.Database,
@@ -168,16 +178,17 @@ func NewInfluxdbMonitor(config InfluxMonitorConfig, logger log.Logger) (Monitor,
 		batchWriteInterval: cfg.BatchWriteInterval,
 		bufferSize:         cfg.BufferSize,
 		maxBufferSize:      cfg.MaxBufferSize,
+
+		done: &sync.WaitGroup{},
 	}
+
+	running := make(chan struct{})
 
 	logger = logger.With(
 		"scheme", cfg.Scheme,
 		"username", cfg.Username,
 		"database", monitor.database,
 		"host", cfg.Host,
-		"batch-write-interval", cfg.BatchWriteInterval.String(),
-		"buffer-size", cfg.BufferSize,
-		"max-buffer-size", cfg.MaxBufferSize,
 	)
 
 	// check connectivity to InfluxDB every 5 minutes
@@ -194,18 +205,36 @@ func NewInfluxdbMonitor(config InfluxMonitorConfig, logger log.Logger) (Monitor,
 					"msg", fmt.Sprintf("couldn't ping influxdb: %v", err),
 				)
 			}
+			select {
+			case <-t.C:
+				// continue
+			case <-running:
+				_ = logger.Info().Log(
+					"during", "influxdb.Client.Ping",
+					"msg", "influxdb monitor closed, stopping influxdb pings",
+				)
+				return
+			}
 
-			<-t.C
 		}
 	}()
 
-	go monitor.batchWriteDaemon()
+	go monitor.batchWriteDaemon(running)
 
 	_ = logger.Info().Log(
 		"msg", fmt.Sprintf("influxdb instrumentation writing to %s://%s@%s/%s", cfg.Scheme, cfg.Username, cfg.Host, monitor.database),
+		"batch-write-interval", cfg.BatchWriteInterval.String(),
+		"buffer-size", cfg.BufferSize,
+		"max-buffer-size", cfg.MaxBufferSize,
 	)
 
-	return monitor, nil
+	return monitor, func() {
+		_ = logger.Debug().Log(
+			"msg", "closing influxdb monitor",
+		)
+		close(running)
+		monitor.done.Wait()
+	}, nil
 }
 
 // InfluxdbMonitor implements monitor.Monitor interface, it wraps
@@ -219,13 +248,24 @@ type influxdbMonitor struct {
 	batchWriteInterval time.Duration
 	bufferSize         int
 	maxBufferSize      int
+
+	// We need a pointer here since:
+	//
+	// > A WaitGroup must not be copied after first use.
+	//
+	// https://godoc.org/sync#WaitGroup
+	done *sync.WaitGroup
 }
 
-func (im influxdbMonitor) batchWriteDaemon() {
+func (im influxdbMonitor) batchWriteDaemon(running chan struct{}) {
+	im.done.Add(1)
 	defer func() {
+		im.done.Done()
+
 		if r := recover(); r != nil {
 			_ = im.logger.Crit().Log(
-				"msg", "batchWriteDaemon panic",
+				"during", "influxdb.influxdbMonitor.batchWriteDaemon",
+				"msg", fmt.Sprintf("panic: %v", r),
 				"recover", r,
 			)
 		}
@@ -248,8 +288,18 @@ func (im influxdbMonitor) batchWriteDaemon() {
 			if len(points) >= nextWriteBufferSize {
 				im.batchWriteAndHandleErr(&points, &nextWriteBufferSize)
 			}
+
+		case <-running:
+			_ = im.logger.Debug().Log(
+				"msg", "influxdb monitor buffer closed, flushing buffer",
+				"point_count", len(points),
+			)
+			im.batchWriteAndHandleErr(&points, &nextWriteBufferSize)
+
+			return
 		}
 	}
+
 }
 
 func increaseBufferSize(nextWriteBufferSize, bufferSize, maxBufferSize int) int {
