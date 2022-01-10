521c106ed902046c54efc0941db225e87cc58cb0
Wenhui Wang <w.eleventh@gmail.com>
2022-01-10 23:23:52 +0800

    Add http_test.go

---
+++ log/trace/http_test.go
@@ -0,0 +1,25 @@
+package trace
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestTraceHTTPRequest(t *testing.T) {
+	req, err := http.NewRequest(http.MethodGet, "https://test/hello", nil)
+	if err != nil {
+		t.Fatalf("err should be nil")
+	}
+
+	resp, err := TraceHTTPRequest(func(r *http.Request) (*http.Response, error) {
+		return &http.Response{StatusCode: http.StatusOK, Status: "200 OK"}, nil
+	}, "testTraceHTTPRequest", req)
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("response status code should be 200")
+	}
+
+	if err != nil {
+		t.Fatalf("err should be nil")
+	}
+}
